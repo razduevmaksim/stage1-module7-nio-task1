b4ae8e21e00c0eac22cb7a4bb634cc95840d5258
mrazduev <mrazduev@gmail.com>
2024-12-09 11:32:04 +0300

    stage1-module7-nio-task1

--- src/main/java/com/epam/mjc/nio/FileReader.java
+++ src/main/java/com/epam/mjc/nio/FileReader.java
@@ -1,11 +1,52 @@
 package com.epam.mjc.nio;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 
 
 public class FileReader {
 
     public Profile getDataFromFile(File file) {
-        return new Profile();
+        StringBuilder txt = new StringBuilder();
+
+        try (RandomAccessFile raf = new RandomAccessFile(file.getPath(), "r"); FileChannel inChannel = raf.getChannel()) {
+            ByteBuffer buff = ByteBuffer.allocate(1024);
+
+            while (inChannel.read(buff) > 0) {
+                buff.flip();
+
+                while (buff.hasRemaining()) {
+                    txt.append((char) buff.get());
+                }
+
+                buff.clear();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+
+        String[] lines = txt.toString().split("\n");
+
+        String name = null;
+        int age = 0;
+        String email = null;
+        long phone = 0;
+
+        for (String line : lines) {
+            if (line.startsWith("Name:")) {
+                name = line.substring(5).trim();
+            } else if (line.startsWith("Age:")) {
+                age = Integer.parseInt(line.substring(4).trim());
+            } else if (line.startsWith("Email:")) {
+                email = line.substring(6).trim();
+            } else if (line.startsWith("Phone:")) {
+                phone = Long.parseLong(line.substring(6).trim());
+            }
+        }
+
+        return new Profile(name, age, email, phone);
     }
 }
